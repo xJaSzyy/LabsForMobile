d10a513d625916deac34edf470d38d491e05165b
xJaSzyy <94278083+xJaSzyy@users.noreply.github.com>
2022-10-02 23:42:24 +0700

    Update Logging.kt

--- Logging.kt
+++ Logging.kt
@@ -1,20 +1,28 @@
 package com.example.logging
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        plant(Timber.DebugTree())
     }
 
-    fun logging() {
+    fun loggingWithLog(view: View) {
         val logText = findViewById<EditText>(R.id.textField).text.toString()
-        Log.v("From EditText", logText)
+        Timber.tag("From EditText").v(logText)
+    }
+
+    fun loggingWithTimber(view: View) {
+        val logText = findViewById<EditText>(R.id.textField).text.toString()
+        Timber.d(logText)
     }
 }
