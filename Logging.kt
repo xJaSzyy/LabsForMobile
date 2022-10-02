30d059d76e453d3a910c5307bda268a70097af3b
xJaSzyy <94278083+xJaSzyy@users.noreply.github.com>
2022-10-02 22:57:03 +0700

    Update Logging.kt

--- Logging.kt
+++ Logging.kt
@@ -2,7 +2,6 @@ package com.example.logging
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,11 +10,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
     }
 
-    fun Logging(view: View) {
-        val logText = findViewById<EditText>(R.id.textField).getText().toString();
-        Log.v("From EditText", logText);
+    fun logging() {
+        val logText = findViewById<EditText>(R.id.textField).text.toString()
+        Log.v("From EditText", logText)
     }
 }
