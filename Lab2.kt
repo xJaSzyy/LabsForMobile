5d8526ab5de267f45401dcbb7acb715b7041fffb
xJaSzyy <94278083+xJaSzyy@users.noreply.github.com>
2022-10-02 12:16:19 +0700

    Update Lab2.kt

--- Lab2.kt
+++ Lab2.kt
@@ -1,1 +1,18 @@
+package com.example.toasthandler
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        val myToast = Toast.makeText(this, "Кнопка ОК", Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+}
