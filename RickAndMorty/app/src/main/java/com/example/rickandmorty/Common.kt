c6f2cf58f180e6d03d46a7dbf52d20b34c2a178c
xJaSzyy <novosel.stepan@gmail.com>
2023-04-09 20:06:54 +0700

    d

--- RickAndMorty/app/src/main/java/com/example/rickandmorty/Common.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rickandmorty
-
-import RetrofitServices
-
-object Common {
-    private const val BASE_URL = "https://rickandmortyapi.com/"
-    val retrofitService: RetrofitServices
-        get() = RetrofitClient.getClient(BASE_URL).create(RetrofitServices::class.java)
-}
