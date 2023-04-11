4954d8f3f1a7d0bc518512eb3174e0ad35e775b6
xJaSzyy <novosel.stepan@gmail.com>
2023-04-11 12:36:17 +0700

    add wf

---
+++ WeatherForecast/app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.weatherforecast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherforecast.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    private val weatherAdapter = WeatherAdapter()
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: ViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Timber.plant(Timber.DebugTree())
+        recycleViewInit()
+
+        viewModel = ViewModelProvider(this)[ViewModel::class.java]
+        viewModel.weatherList.observe(this) {
+            weatherAdapter.submitList(it)
+        }
+    }
+
+    private fun recycleViewInit() {
+        binding.rvWeather.adapter = weatherAdapter
+        binding.rvWeather.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+    }
+}
